b3783c296b3faa9e90a5bfd8d4d3203cd500cef2
JaelNgutu <jaelngutu@gmail.com>
2022-03-10 23:12:38 +0300

    add html file

--- src/main/java/app/ultratechies/api/Application.java
+++ src/main/java/app/ultratechies/api/Application.java
@@ -2,10 +2,18 @@ package app.ultratechies.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 
 public class Application {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
