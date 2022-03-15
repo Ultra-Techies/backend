0ef63a48cce618b1ad15216451682adf825e6332
Ian Njari <iannjari@gmail.com>
2022-03-15 10:56:19 +0300

    add user authentication

---
+++ src/main/java/app/ultratechies/api/AppUsers/UserAuthController.java
@@ -0,0 +1,28 @@
+package app.ultratechies.api.AppUsers;
+
+
+import app.ultratechies.api.AppUsers.UserDTO.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/user/auth")
+public class UserAuthController {
+
+    private final UserAuthService userAuthService;
+
+    @Autowired
+    public UserAuthController(UserAuthService userAuthService) {
+        this.userAuthService = userAuthService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<Optional<UserDto>> getUser(@RequestBody AppUser appUser){
+        Optional<UserDto> appuser=userAuthService.authUser(appUser.getUsername(), appUser.getPassword());
+        return ResponseEntity.ok(appuser);
+    }
+}
+
