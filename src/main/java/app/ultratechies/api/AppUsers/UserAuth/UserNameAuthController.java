070d68a22c62614211b1ca435cdffeb058774db1
Ian Njari <iannjari@gmail.com>
2022-03-16 13:19:35 +0300

    add crossorigin

--- src/main/java/app/ultratechies/api/AppUsers/UserAuth/UserNameAuthController.java
+++ src/main/java/app/ultratechies/api/AppUsers/UserAuth/UserNameAuthController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/user/name/auth")
 public class UserNameAuthController {
--- src/main/java/app/ultratechies/api/AppUsers/UserController.java
+++ src/main/java/app/ultratechies/api/AppUsers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path = "api/user")
 public class UserController {
